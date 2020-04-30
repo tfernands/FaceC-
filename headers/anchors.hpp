ab4e4a3191cff2942fb4e3a19d57e3809aab1d65
tfernands <thalesfernandesdasilva@gmail.com>
2020-04-29 23:22:40 -0300

    Added class Vec2

--- headers/anchors.hpp
+++ headers/anchors.hpp
@@ -6,11 +6,21 @@
 #include "xtensor/xview.hpp"
 #include <float.h>
 #include "util.hpp"
+#include "Vec2.hpp"
 
 class Anchors{
+	private:
+
+		static Anchors create_anchors(float size, xt::xarray<float> aspect_ratios, xt::xarray<float> scales){
+			xt::xarray<float> anchors = xt::zeros<float>({aspect_ratios.shape()[0], scales.shape()[0], 4});
+
+
+		}
 
 	public:
-		Anchors(float size, xt::xarray<float> aspect_ratios, xt::xarray<float> scales, xt::xarray<float> dimensions, xt::xarray<float> fmap_size){
+
+
+		Anchors(float size, xt::xarray<float> aspect_ratios, xt::xarray<float> scales, Vec2 dimensions, Vec2 fmap_size){
 			
 		}
 
--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,10 @@
 #include "anchors.hpp"
 
 int main(){
-	//Anchors a(32, xt::xarray<float>{1, 0.5}, xt::xarray<float>{1, 0.5}, (2, 2))
+	Anchors abc(32, xt::xarray<float>({1, 0.5}), xt::xarray<float>({1, 0.5}), Vec2(2, 2), Vec2(2,2));
 	xt::xarray<float> a = {{1,4,6,7},{0.8,0.4,2,3}};
 	xt::xarray<float> b = {{1,5,2,6},{3,2,4,3}};
 	std::cout<<iou(a, b)<<std::endl;
+	std::cout<<xt::zeros<float>({2, 3, 4})<<std::endl;
 	return 0;
 }
