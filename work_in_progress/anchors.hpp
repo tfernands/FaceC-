ec95d4bd4fa40771c971701592dce25f0e0ae235
tfernands <thalesfernandesdasilva@gmail.com>
2020-04-30 12:10:06 -0300

    commit

---
+++ work_in_progress/anchors.hpp
@@ -0,0 +1,20 @@
+#ifndef ANCHORSHPP
+#define ANCHORSHPP
+
+#include "xtensor/xarray.hpp"
+#include "xtensor/xio.hpp"
+#include "xtensor/xview.hpp"
+#include <float.h>
+#include "util.hpp"
+
+class Anchors{
+
+	public:
+		Anchors(float size, xt::xarray<float> aspect_ratios, xt::xarray<float> scales, xt::xarray<float> dimensions, xt::xarray<float> fmap_size){
+			
+		}
+
+};
+
+
+#endif
