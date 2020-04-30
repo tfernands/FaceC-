d445fc654e6f981f91564758f9861515d36549e3
tfernands <thalesfernandesdasilva@gmail.com>
2020-04-30 12:47:13 -0300

    Added header tracking.hpp

--- include/Vec2.hpp
+++ include/Vec2.hpp
@@ -1,69 +1,29 @@
 #ifndef VEC2HPP
 #define VEC2HPP
 
+#include <fstream>
+
 class Vec2{
 	
 	public:
 		float e[2];
-		Vec2(float x, float y){
-			e[0] = x;
-			e[1] = y;
-		}
 
-		virtual ~Vec2(){}
+		Vec2(float x, float y);
+		virtual ~Vec2();
 
-		inline Vec2& set(float e0, float e1){
-			e[0] = e0;
-			e[1] = e1;
-			return *this;
-		}
-
-		inline float operator[](int i) const {
-			return e[i];
-		}
-
-		inline float& operator[](int i) {
-			return e[i];
-		}
-
-		inline float x() const {
-			return e[0];
-		}
-
-		inline float y() const {
-			return e[1];
-		}
-
-		inline Vec2& operator+=(const Vec2 &v){
-			e[0] += v.e[0];
-			e[1] += v.e[1];
-			return *this;
-		}
-
-		inline Vec2& operator-=(const Vec2 &v){
-			e[0] -= v.e[0];
-			e[1] -= v.e[1];
-			return *this;
-		}
-
-		friend inline Vec2 operator+(const Vec2 &v1, const Vec2 &v2){
-			return Vec2(v1.e[0]+v2.e[0], v1.e[1]+v2.e[1]);
-		}
-
-		friend inline Vec2 operator-(const Vec2 &v1, const Vec2 &v2){
-			return Vec2(v1.e[0]-v2.e[0], v1.e[1]-v2.e[1]);
-		}
-
-		friend inline Vec2 operator-(const Vec2 &v){
-			return Vec2(-v.e[0], -v.e[1]);
-		}
-
-		friend std::ostream& operator<<(std::ostream &out, Vec2 &v){
-			out << "[" << v.e[0] << ", " << v.e[1] << "]";
-			return out;
-		}
+		inline Vec2& set(float e0, float e1);
+		inline float operator[](int i) const;
+		inline float& operator[](int i);
+		inline float x() const;
+		inline float y() const;
+		inline Vec2& operator+=(const Vec2 &v);
+		inline Vec2& operator-=(const Vec2 &v);
 };
 
+inline Vec2 operator+(const Vec2 &v1, const Vec2 &v2);
+inline Vec2 operator-(const Vec2 &v1, const Vec2 &v2);
+inline Vec2 operator-(const Vec2 &v);
+std::ostream& operator<<(std::ostream &out, Vec2 &v);
 
 #endif
 
---
+++ include/tracking.hpp
@@ -0,0 +1,34 @@
+#ifndef ANCHORSHPP
+#define ANCHORSHPP
+
+#include "xtensor/xarray.hpp"
+#include "xtensor/xio.hpp"
+#include "xtensor/xview.hpp"
+#include "xtensor/xfixed.hpp"
+#include "xtensor/xadapt.hpp"
+#include "xtensor/xpad.hpp"
+#include "xtensor/xstrided_view.hpp"
+#include "xtensor/xmanipulation.hpp"
+#include "xtensor/xsort.hpp"
+#include "xtensor/xindex_view.hpp"
+#include <float.h>
+#include "util.hpp"
+#include "Vec2.hpp"
+
+class Focus{
+	private:
+		float mean_y;
+		float mean_x;
+		float border;
+		float resting_border;
+		std::tuple<float, float> resting_center;
+
+	public:
+		Focus(xt::xarray<float> frame, float border=0.1);
+		virtual ~Focus();
+
+		void update(xt::xarray<float> bboxes, xt::xarray<float> frameh, float target_ratio);
+		std::tuple<xt::xarray<float>, std::tuple<int, int, int, int, int>> crop(xt::xarray<float> frame);
+};
+
+#endif
